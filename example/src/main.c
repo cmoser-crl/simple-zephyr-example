11b4ace9a244e23044fd2623ad0b2440b0f534d9
cmoser <cmoser@carnegierobotics.com>
2025-03-06 12:33:51 -0500

    add simple minimal zephyr example.

---
+++ example/src/main.c
@@ -0,0 +1,25 @@
+#include <zephyr/kernel.h>
+
+K_TIMER_DEFINE(timer, NULL, NULL);
+
+uint32_t iters = 0;
+uint32_t counter = 0;
+
+int main(void)
+{
+    k_timer_start(&timer, K_MSEC(0), K_MSEC(5));
+
+    while (1)
+    {
+        k_timer_status_sync(&timer);
+        // k_sleep(K_MSEC(5));
+        if (++counter > 19)
+        {
+            printk("hello world %u\n", ++iters);
+            counter = 0;
+        }
+    }
+
+    return 0;
+}
+
